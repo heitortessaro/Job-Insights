3da73968d846e7523bded0c070230507112725ab
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-10-21 17:29:49 -0300

    test: add test for test_brazilian_jobs function
    it verifies if the returned dict has keys in english

--- tests/brazilian/test_brazilian_jobs.py
+++ tests/brazilian/test_brazilian_jobs.py
@@ -1,5 +1,9 @@
-# from src.brazilian_jobs import read_brazilian_file
+from src.brazilian_jobs import read_brazilian_file
 
 
 def test_brazilian_jobs():
-    pass
+    jobs_list = read_brazilian_file("tests/mocks/brazilians_jobs.csv")
+    job_keys_in_english = jobs_list[0].keys()
+    assert "title" in job_keys_in_english
+    assert "salary" in job_keys_in_english
+    assert "type" in job_keys_in_english
