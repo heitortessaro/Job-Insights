2525856a5dc658b1fa0783a1b3ba1580b5a9c47e
Heitor Jose Tessaro <heitor.j.tessaro@gmail.com>
2022-10-21 16:56:39 -0300

    test: Create test for test_counter function

--- tests/counter/test_counter.py
+++ tests/counter/test_counter.py
@@ -1,5 +1,22 @@
-# from src.counter import count_ocurrences
+import pytest
+from unittest.mock import mock_open, patch
+from src.counter import count_ocurrences
 
 
-def test_counter():
-    pass
+@pytest.fixture
+def jobs_text():
+    return """ Testing text with 3 Python python PythOn,
+            2 Javascript JavaScript and 1 Trybe"""
+
+
+def test_counter(jobs_text):
+    # more about patch:
+    # https://docs.python.org/3/library/unittest.mock.html#unittest.mock.patch
+    # quick guide:
+    # https://docs.python.org/3/library/unittest.mock.html#quick-guide
+    # mock_open:
+    # https://docs.python.org/3/library/unittest.mock.html#mock-open
+    with patch("builtins.open", mock_open(read_data=jobs_text)):
+        assert count_ocurrences("/fake/path/fake.csv", "python") == 3
+        assert count_ocurrences("/fake/path/fake.csv", "javascript") == 2
+        assert count_ocurrences("/fake/path/fake.csv", "trybe") == 1
